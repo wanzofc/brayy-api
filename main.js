6e0f8e330f7770d6ae3c51946b79530e82c09365
wanzofc <berlianawan498@gmail.com>
2025-01-15 12:10:37 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,27 @@ const PORT = 3000;
 
 const mainrouter = require("./routes/mainrouter.js"),
 	apirouter = require("./routes/api.js");
-
+const SECRET_KEY = "6LfmBbgqAAAAADUUDsvnYqXtUWcsOk5MOnw491gg";
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post("/verify-captcha", async (req, res) => {
+  const token = req.body["g-recaptcha-response"]; // Ambil token dari form
+  if (!token) {
+    return res.status(400).send("Token reCAPTCHA tidak ditemukan.");
+  }
+  try {
+    const response = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${SECRET_KEY}&response=${token}`
+    );
+    if (response.data.success) {
+      res.send("reCAPTCHA berhasil diverifikasi.");
+    } else {
+      res.status(400).send("Verifikasi reCAPTCHA gagal.");
+    }
+  } catch (error) {
+    console.error("Error saat memverifikasi reCAPTCHA:", error);
+    res.status(500).send("Terjadi kesalahan saat memverifikasi reCAPTCHA.");
+  }
+});
 app.enable('trust proxy');
 app.set("json spaces", 2);
 app.use(cors());
